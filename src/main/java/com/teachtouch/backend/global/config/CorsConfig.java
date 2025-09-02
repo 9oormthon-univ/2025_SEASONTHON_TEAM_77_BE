cc39ea65f2d29618fc0d9e8c8f1e8d83511970bf
sch0426 <sch04265@gmail.com>
2025-09-02 17:23:47 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/teachtouch/backend/global/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.teachtouch.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // 모든 도메인 허용
+                .allowedMethods("*") // 허용 HTTP 메소드
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(true); //
+    }
+}
